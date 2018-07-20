29218ef4691dd6208c0fbfecd8bced09b6d89a52
James Hanna <jhanna@kx.com>
2018-07-20 18:43:24 +0100

    debug added to p.q conda build only with python 3.6

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -5,7 +5,7 @@ move p.k %QHOME% || goto :error
 
 set QBIN=%QHOME%\w64
 mkdir QBIN
-cl /LD /DKXVER=3 /Fep.dll /02 py.c q.lib || goto :error
+cl /LD /DKXVER=3 /Fep.dll /O2 py.c q.lib || goto :error
 move p.dll %QBIN% || goto :error
 exit 0
 :error
