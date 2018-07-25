1741adbdb315f7d57fa7c3372fe3c350c50f65e3
James Hanna <jhanna@kx.com>
2018-07-25 15:05:27 +0100

    move conda tests to batch file

---
+++ conda-recipe/test.bat
@@ -0,0 +1,16 @@
+if not defined QLIC (
+ goto :nokdb
+)
+conda create -y -n conda_test -c kx/label/dev kdb || goto :error
+activate conda_test || goto :error
+conda install -y --use-local embedpy || goto :error
+conda install --file tests\requirements.txt || goto :error
+q test.q || goto :error
+exit /b 0
+
+:error
+exit /b %errorLevel%
+
+:nokdb
+echo no kdb
+exit /b 0
