3b89b6ed31c3e05997364a24376b0baf8e663b2b
James Hanna <jhanna@kx.com>
2018-07-25 15:58:19 +0100

    test

--- conda-recipe/test.bat
+++ conda-recipe/test.bat
@@ -1,10 +1,6 @@
 if not defined QLIC (
  goto :nokdb
 )
-echo create environment
-conda create -y -n conda_test -c kx/label/dev kdb || goto :error
-echo activate environment
-activate conda_test || goto :error
 echo install embedpy
 conda install -y --use-local embedpy || goto :error
 echo install requirements
