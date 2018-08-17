a9b1b8ba8a789a5c86a88ea51a53e889cd684c85
James Hanna <jhanna@kx.com>
2018-08-17 15:23:54 +0100

    debug

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,9 +1,12 @@
 if not defined QLIC (
  goto :nokdb
 )
-q conda-recipe/prep_requirements.q -q
+q conda-recipe/prep_requirements.q -q || goto :error
+echo installing
 conda install -y -q --file tests\requirements_filtered.txt
+echo installed
 q test.q -q || goto :error
+echo tests run
 exit /b 0
 
 :error
