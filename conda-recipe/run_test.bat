9d3b91226bf2811f6a64cbe057147dc28c66bbef
James Hanna <jhanna@kx.com>
2018-08-17 15:56:34 +0100

    q is a batch file in conda on windows

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,12 +1,9 @@
 if not defined QLIC (
  goto :nokdb
 )
-q conda-recipe/prep_requirements.q -q || goto :error
-echo installing
+call q conda-recipe/prep_requirements.q -q || goto :error
 conda install -y -q --file tests\requirements_filtered.txt
-echo installed
 q test.q -q || goto :error
-echo tests run
 exit /b 0
 
 :error
